8e32af430897a9a744c1f1f261c2ba6cb83c5383
T. Korn <email@tobiaskorn.de>
2023-06-27 16:29:44 +0200

    swtched from KornSw.EntityAnnotations to FUSE-fs.EntityAnnotations and added net 6.0 target

--- Lib.net461/Properties/AssemblyInfo.cs
+++ Lib.net461/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("46adc8b8-2f14-4c31-87ca-95b71ee09ef9")]
 
-[assembly: AssemblyVersion("1.2.0.*")]
-[assembly: AssemblyInformationalVersion("1.2.0-localbuild")]
+[assembly: AssemblyVersion("1.3.0.*")]
+[assembly: AssemblyInformationalVersion("1.3.0-localbuild")]
 //DONT ADD: [assembly: AssemblyFileVersion] !!!
